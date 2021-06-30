76ae3feebb9131f4a0e324f660aa85276bec34e9
Alice-Retz <alicegretz@gmail.com>
2021-06-30 17:19:11 -0500

    Click counter functional.

--- app.js
+++ app.js
@@ -4,9 +4,41 @@
 //read user input
 //if else statement
 //relevant response
+import { getRandomInt } from './utils.js';
 
+//const resetBtn = document.getElementById('reset-button');
 const numberInput = document.getElementById('user-guess');
 const submitBtn = document.getElementById('submit-button');
-const resetBtn = document.getElementById('reset-button');
 const response = document.getElementById('response');
+const attempts = document.getElementById('attempts-left');
+const randNum = getRandomInt();
+var clickCount = 0;
+console.log(randNum);
+// resetBtn.addEventListener('click', ()=>{
+//     console.log(randNum);
+//     return randNum;
+//     clickCount =0;
+// });
+
+
+submitBtn.addEventListener('click', ()=>{
+    clickCount++;
+    console.log(clickCount);
+    if (clickCount > 2) {
+        document.getElementById('submit-button').disabled = true;
+        return attempts.textContent = `You're out of guesses!`;
+    } else {
+        return attempts.textContent = `You've guessed ${clickCount}/3 times.`;
+    }
+});
+
+submitBtn.addEventListener('click', ()=>{
+    if (numberInput.value > randNum) {
+        return response.textContent = `Lower.`;
+    } else if (numberInput.value < randNum) {
+        return response.textContent = `Higher.`;
+    } else {
+        return response.textContent = `You guessed it!`;
+    }
+});
 
---
+++ utils.js
@@ -0,0 +1,5 @@
+//Random Number Function provided by MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+export function getRandomInt() {
+    return Math.floor(Math.random() * 20);
+}
+
